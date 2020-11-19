284cc4bed84ff5846a8ad94fa7c24646a8e7c058
DIMTRI ALBANO DE OLIVEIRA <dimtri.oliveira@fatec.sp.gov.br>
2020-11-18 22:17:01 -0300

    Botao de Login

--- prontuario/app.js
+++ prontuario/app.js
@@ -3,7 +3,6 @@ var btnSignup = document.querySelector("#signup");
 
 var body = document.querySelector("body");
 
-
 btnSignin.addEventListener("click", function () {
    body.className = "sign-in-js"; 
 });
